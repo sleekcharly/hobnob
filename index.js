0e45fcc612ab43be27046429002ae644f07d585d
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-08-29 05:41:23 +0100

    fixed typo errors in index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const http = require("http");
 
 // initialize request handler
 const requestHandler = function (req, res) {
-  res.writeHead(200, { "content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, World!");
 };
 
 // initialize server with request handler function
 const server = http.createServer(requestHandler);
 // listen for incoming connection
-server.listen(8000);
+server.listen(8080);
 console.log(`server listening on: ` + 8080);
